69bd9a48f6a6b3db90374848df5e0f3579b57786
ADMIN <admin@Anjiths-MacBook-Air.local>
2024-06-03 22:55:03 +0530

    1st checkpoint

---
+++ lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:app1/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color.fromRGBO(212, 197, 238, 1), Colors.deepPurple],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
